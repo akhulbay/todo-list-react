129e82e9b22ad49619f427e8047f70edda5ce5b9
T1m4ay <tero4y@gmail.com>
2023-02-10 20:58:42 +0600

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Menu/Menu.module.css 	modified:   src/components/Menu/MenuBar.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Header from "./components/Header/Header";
 import MenuBar from "./components/Menu/MenuBar";
 import TodoList from "./components/TodoList/TodoList";
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "First todo",
+      status: true,
+    },
+    {
+      id: 2,
+      title: "Second todo",
+      status: true,
+    },
+    {
+      id: 3,
+      title: "Third todo",
+      status: true,
+    },
+  ]);
 
-    const [todo, setTodo] = useState([
-        {
-            id: 1,
-            title: 'First todo',
-            status: true
-        },
-        {
-            id: 2,
-            title: 'Second todo',
-            status: true
-        },
-        {
-            id: 3,
-            title: 'Third todo',
-            status: true
-        }
-    ])
-
-    return (
-        <div className="App">
-            <Header/>
-            <MenuBar/>
-            <TodoList/>
-        </div>
+  return (
+    <div className="App">
+      <Header />
+      <div className="AppBody">
+        <MenuBar />
+        <TodoList />
+      </div>
+    </div>
   );
 }
 
--- src/components/Menu/MenuBar.js
+++ src/components/Menu/MenuBar.js
@@ -1,11 +1,21 @@
 import ms from "./Menu.module.css";
+import "./Menu.module.css";
 
 function MenuBar() {
-    return (
-        <div className={ms.MenuBar}>
-
-        </div>
-    )
+  return (
+    <div className={ms.MenuBar}>
+      <div className={ms.TextBody}>
+        <ul className={ms.ul}>
+          <li>Tags</li>
+          <li>All</li>
+          <li>University/Work</li>
+          <li>Home</li>
+          <li>Personal</li>
+          <li>Payment</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default MenuBar;
