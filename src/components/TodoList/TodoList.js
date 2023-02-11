8bf99d5a6694a7f4a8f1f823b75beed9ba69623e
akhulbay <akhulbay.shyngys@gmail.com>
2023-02-11 09:25:50 +0600

    Some functionality on TodoList blockg

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,12 +1,39 @@
 import ts from "./TodoList.module.css";
 import AddTodo from "./AddTodo/AddToDo";
+import {useState} from "react";
 
 function TodoList({ todo, setTodo }) {
+
+    const [edit, setEdit] = useState(null);
+    const [value, setValue] = useState('')
 
     function deleteToDo(id) {
         let newToDo = [...todo].filter(item => item.id != id);
         setTodo(newToDo);
     }
+
+    function editTodo(id, title) {
+        setEdit(id);
+        setValue(title);
+    }
+
+    function saveTodo(id) {
+        let newTodo = [...todo].map(item => {
+            if (item.id == id) {
+                item.title = value;
+            }
+            return item ;
+        })
+        setTodo(newTodo);
+        setEdit(null);
+    }
+
+    function handleKeyUp(e) {
+        if (e.key === "Enter"){
+
+        }
+    }
+
     return (
         <div className={ts.TodoList}>
             <div className={ts.container}>
@@ -14,8 +41,27 @@ function TodoList({ todo, setTodo }) {
                     {
                         todo.map(item => (
                             <div key={item.id}>
-                                <div>{item.title}</div>
-                                <button onClick={ () => {deleteToDo(item.id)}}>Удалить</button>
+                                {
+                                    edit === item.id ?
+                                        <div>
+                                            <input onChange={ (e) => setValue(e.target.value)} onKeyUp={(e)=>handleKeyUp(e)} value={value}/>
+                                        </div>
+                                        :
+                                        <div>
+                                            {item.title}
+                                        </div>
+                                }
+                                {
+                                    edit === item.id ?
+                                        <div>
+                                            <button onClick={ () => saveTodo(item.id)}>Сохранить</button>
+                                        </div>
+                                        :
+                                        <div>
+                                            <button onClick={ () => {deleteToDo(item.id)}}>Удалить</button>
+                                            <button onClick={ () => {editTodo(item.id, item.title)}}>Edit</button>
+                                        </div>
+                                }
                             </div>
                         ))
                     }
