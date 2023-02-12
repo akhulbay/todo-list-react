f67b48d045fadd0602fa6d279a07819724028ec2
whynur <dauletova.ainur2801@gmail.com>
2023-02-12 11:16:28 +0600

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,25 @@ import hs from "./Header.module.css";
 function Header() {
     return (
         <div className={hs.Header}>
-            <div className={hs.container}>
-                <h1>TodoList</h1>
+            <div className={hs.Container}>
+                <img src="homepage.png" alt="" className={hs.Homepage}/>
+                <form id = "form" >
+                    <input type="search" id="query" name="q" placeholder="Search" className = {hs.Search}/>
+                    <img src="lupadlypoiska.png" alt="" className={hs.Searchicon}/>
+                </form>
+                <div className={hs.Container2}></div>
+                <button type="button" className={hs.Percentage}>
+                    <img src="percentage.png" alt="" className={hs.Percentageicon}/>
+                </button>
+                
+                <button type = "button" className={hs.Question}>
+                    <img src="questions.png" alt="" className={hs.Questionicon}/>
+                </button>
+
+                <button type = "button" className={hs.Account}>
+                    <img src="account.png" alt="" className={hs.Accounticon}/>
+                </button>
+
             </div>
         </div>
     )
