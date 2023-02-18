a96397ded5a06305b30a34f1350558b2baf2a715
whynur <dauletova.ainur2801@gmail.com>
2023-02-18 09:17:08 +0600

    header-header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,28 @@
 import hs from "./Header.module.css";
-function Header() {
+import {useState} from "react";
+function Header()  {
+
     return (
         <div className={hs.Header}>
             <div className={hs.Container}>
                 <img src="homepage.png" alt="" className={hs.Homepage}/>
                 <form id = "form" >
-                    <input type="search" id="query" name="q" placeholder="Search" className = {hs.Search}/>
+                    <input type="search" id="query"
+                           name="q"
+                           placeholder="Search"
+                           className = {hs.Search}
+
+                    />
                     <img src="lupadlypoiska.png" alt="" className={hs.Searchicon}/>
                 </form>
+
                 <div className={hs.Container2}></div>
                 <button type="button" className={hs.Percentage}>
                     <img src="percentage.png" alt="" className={hs.Percentageicon}/>
                 </button>
                 
                 <button type = "button" className={hs.Question}>
-                    <img src="questions.png" alt="" className={hs.Questionicon}/>
+                    <img src="galochka.png" alt="" className={hs.Questionicon}/>
                 </button>
 
                 <button type = "button" className={hs.Account}>
